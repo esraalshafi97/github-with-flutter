bbb3f910197e843c8e17776777aa6ab5817f0722
esra alshafi <esra.alshafi@gmail.com>
2022-08-26 23:21:17 +0200

    login final view

--- lib/app/modules/logIn/views/log_in_view.dart
+++ lib/app/modules/logIn/views/log_in_view.dart
@@ -11,47 +11,46 @@ class LogInView extends GetView<LogInController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LogInView'),
-        centerTitle: true,
-      ),
+
       body: GetX<LogInController>(
         builder: (_) {
           if(!controller.isLoaded.value){
             return LoadingWidget();
           }
-          return Column(
-            children: [
-              SizedBox(height: Get.height*.2,),
-              Image.asset("assets/github.png",width:Get.width*.6),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                    width: Get.width*.6,child: Text("Welcom to Repo Viwee",style: TextStyle(fontSize: 26,),textAlign: TextAlign.center,)),
-              ),
-
-              Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: Get.height*.2,),
+                Image.asset("assets/github.png",width:Get.width*.6),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Container(
-                    width: Get.width*.6,
-                      child: Center(child: Text("SignIn"))),
-                  onPressed: ()async{
-                    UserCredential _user= await controller.signInWithGitHub(context);
-                    if(_user.user!=null){
-
-                      Get.offAllNamed( Routes.HOME);
-
-                    }
-
-                  },
+                      width: Get.width*.6,child: Text("Welcom to Repo Viwee",style: TextStyle(fontSize: 26,),textAlign: TextAlign.center,)),
                 ),
-              )
+
+                Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
+                    child: Container(
+                      width: Get.width*.6,
+                        child: Center(child: Text("SignIn"))),
+                    onPressed: ()async{
+                      UserCredential _user= await controller.signInWithGitHub(context);
+                      if(_user.user!=null){
+
+                        Get.offAllNamed( Routes.HOME);
+
+                      }
+
+                    },
+                  ),
+                )
 
         ),
-            ],
+              ],
+            ),
           );
         }
       ),
